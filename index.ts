4e76716867d2c2d5997019d2129286d501a16e3e
fraxion <fraxion26@gmail.com>
2025-08-22 15:52:36 +0200

    remove tsc script

--- index.ts
+++ index.ts
@@ -3,14 +3,9 @@ import Express from 'express';
 import router from './routes/index';
 import path from "node:path";
 
-// Création d'une instance de l'application Express
-// qui va gérer les requêtes HTTP entrantes
-// et les rediriger vers les routes définies dans le routeur.
+
 const app = Express();
 
-// Définition du port sur lequel le serveur va écouter
-// les requêtes HTTP. Ici, nous utilisons le port 3000.
-const PORT = 3006;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,15 +13,12 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Express.json() permet de convertir le
-// corps de la requête au format JSON
+
 app.use(Express.json());
 
 app.use(Express.static(path.join(__dirname, "public")))
 
 
-// Utilisation du routeur défini dans le fichier `routes/index.ts`
-// qui contient toutes les routes de l'application
 app.use(router);
 
 export default app;
